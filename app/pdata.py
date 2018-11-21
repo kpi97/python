e3ff7285457aed22430c4f6b25e8b7d33c2580ad
U-AMM\pominova_aa <pominova_aa@c1r216n11.amm.vsu.ru>
2018-11-21 16:18:22 +0300

    first commit

--- app/pdata.py
+++ app/pdata.py
@@ -4,6 +4,6 @@ proxy_ip = "109.74.136.78"
 proxy_port = 4145
 
 from_smpt = "smtp.mail.ru"
-from_email = "nikita.zharkovsky_1@mail.ru"
-from_key = "123456a"
-to_email = "nikita.zharkovsky_2@mail.ru"
+from_email = "alena.pominova_1@mail.ru"
+from_key = "1234@qwe"
+to_email = "alena.pominova_2@mail.ru"
