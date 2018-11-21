52fbd4c38ca534f2b6e13815ce05c98a525c0c4d
U-AMM\zharkovskij_ne <zharkovskij_ne@c1r216n12.amm.vsu.ru>
2018-11-21 15:43:42 +0300

    init

--- app/pdata.py
+++ app/pdata.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
-# coding=utf8
-
+#!/bin/env python3
+# -*- coding: utf-8 -*-
 proxy_ip = "109.74.136.78"
 proxy_port = 4145
 
-from_smtp = "smtp.mail.ru"
-# from_smtp = "217.69.139.160"
-
-# from_smtp = "smtp.yandex.ru"
-
-# from_email = "vsu-python1@yandex.ru"
-from_email = "vsu-python1@mail.ru"
-from_key = "123456A"
-# to_email = "vsu-python1@mail.ru"
-to_email = "vsu-python1@yandex.ru"
-
+from_smpt = "smtp.mail.ru"
+from_email = "nikita.zharkovsky_1@mail.ru"
+from_key = "123456a"
+to_email = "nikita.zharkovsky_2@mail.ru"
